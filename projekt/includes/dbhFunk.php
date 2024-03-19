b73c7b173f55e1daec9b0e6eba795456c4132b1e
MarcusJak <78412619+MarcusJak@users.noreply.github.com>
2024-03-19 12:12:59 +0100

    Update dbhFunk.php

--- projekt/includes/dbhFunk.php
+++ projekt/includes/dbhFunk.php
@@ -1,11 +1,11 @@
 <?php
 
 $serverName='studentmysql.miun.se';
-$dbUsername='maja2003';
-$dbUserPassword='dsck6ly5';
-$dbName='maja2003';
+$dbUsername='';
+$dbUserPassword='';
+$dbName='';
 //kopplar till databasen
-$conn=new mysqli('studentmysql.miun.se', 'maja2003', 'dsck6ly5', 'maja2003');
+$conn=new mysqli();
 //$conn=new mysqli('localhost', 'root', '', 'projekt');
 
 if(!$conn)
